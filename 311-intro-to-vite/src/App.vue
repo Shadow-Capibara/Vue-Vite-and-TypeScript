c2120e464ddcfa698412578205b3bee565431814
yosakiva <138710107+yosakiva@users.noreply.github.com>
2024-07-11 22:24:53 +0700

    8.6

--- 311-intro-to-vite/src/App.vue
+++ 311-intro-to-vite/src/App.vue
@@ -17,7 +17,7 @@ import { RouterLink, RouterView } from 'vue-router'
   
 </template>
 
-<style scoped>
+<style >
   #layout{
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -37,6 +37,10 @@ nav a {
 
 nav a.router-link-exact-active{
   color: #42b983;
+}
+
+h2{
+  font-size: 20px;
 }
 
 </style>
